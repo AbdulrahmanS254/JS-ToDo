92fc32bac3d8f6150bcd30cdca19e49826d8b1a5
Abdulrahman Shawky <abdulrahman.shawkym@gmail.com>
2025-04-22 13:47:11 +0200

    semi finished

--- main.js
+++ main.js
@@ -2,17 +2,56 @@ const form = document.getElementById("add-form");
 const inputBox = document.getElementById("input-box");
 const addBtn = document.getElementById("add-btn");
 const tasksBox = document.getElementById("task-container");
+const close = document.getElementsByClassName("close-btn");
+const taskItem = document.getElementsByClassName("task-item");
+
+let taskArr = [];
 
 form.addEventListener("submit", (ev) => {
+    // making sure the input is not empty
     if (inputBox.value === "") {
         inputBox.classList.add("wrong-input");
         inputBox.addEventListener("animationend", () => {
             inputBox.classList.remove("wrong-input");
         });
     } else {
-        let task = document.createElement('li');
+        let task = document.createElement("li");
+        task.classList.add("task-item");
+        let del = document.createElement("span");
+        del.innerHTML = "\u00d7";
+        del.classList.add("del-btn");
         task.innerText = inputBox.value;
+        task.appendChild(del);
         tasksBox.appendChild(task);
     }
+    inputBox.value = "";
     ev.preventDefault();
+    saveData();
 });
+
+tasksBox.addEventListener(
+    "click",
+    (e) => {
+        if (e.target.classList.contains("task-item")) {
+            e.target.classList.toggle("checked");
+            saveData();
+            return;
+        }
+        if (e.target.className === "del-btn") {
+            e.target.parentElement.remove();
+            saveData();
+            return;
+        }
+    },
+    false
+);
+
+const saveData = () => {
+    localStorage.setItem("data", tasksBox.innerHTML);
+};
+
+const getData = () => {
+    tasksBox.innerHTML = localStorage.getItem("data");
+};
+
+getData();
