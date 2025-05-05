f870be7312ca8bba13ee159dab1dadca26dbefa1
Abdulrahman Shawky <abdulrahman.shawkym@gmail.com>
2025-05-06 01:43:28 +0300

    finished

--- main.js
+++ main.js
@@ -5,40 +5,82 @@ const tasksBox = document.getElementById("task-container");
 const close = document.getElementsByClassName("close-btn");
 const taskItem = document.getElementsByClassName("task-item");
 
-let taskArr = [];
+window.addEventListener("DOMContentLoaded", () => {
+    getData();
+});
+
+let tasksData = [];
+
+const dataObjCreate = (inputValue) => {
+    let obj = {};
+    obj.id = Date.now() + Math.floor(Math.random() * 1000);
+    obj.text = inputValue;
+    obj.complete = false;
+    tasksData.push(obj);
+};
+
+const renderTask = () => {
+    tasksBox.innerHTML = "";
+    tasksData.forEach((el) => {
+        let task = document.createElement("li");
+        task.classList.add("task-item");
+        task.setAttribute("data-id", el.id);
+        task.innerText = el.text;
+
+        let del = document.createElement("span");
+        del.classList.add("del-btn");
+        del.innerHTML = "\u00d7";
+        del.setAttribute("data-id", el.id);
+        task.appendChild(del);
+        tasksBox.appendChild(task);
+        if (el.complete === true) {
+            task.classList.add("checked");
+        }
+    });
+};
 
 form.addEventListener("submit", (ev) => {
     // making sure the input is not empty
-    if (inputBox.value === "") {
-        inputBox.classList.add("wrong-input");
-        inputBox.addEventListener("animationend", () => {
-            inputBox.classList.remove("wrong-input");
-        });
-    } else {
-        let task = document.createElement("li");
-        task.classList.add("task-item");
-        let del = document.createElement("span");
-        del.innerHTML = "\u00d7";
-        del.classList.add("del-btn");
-        task.innerText = inputBox.value;
-        task.appendChild(del);
-        tasksBox.appendChild(task);
-    }
-    inputBox.value = "";
     ev.preventDefault();
+    let text = inputBox.value.trim();
+    if (text === "") {
+        triggerAni();
+    } else {
+        dataObjCreate(text);
+        renderTask();
+    }
+    // emptying the input
+    inputBox.value = "";
     saveData();
 });
+
+const triggerAni = () => {
+    inputBox.classList.add("wrong-input");
+    inputBox.addEventListener("animationend", () => {
+        inputBox.classList.remove("wrong-input");
+    });
+};
 
 tasksBox.addEventListener(
     "click",
     (e) => {
-        if (e.target.classList.contains("task-item")) {
-            e.target.classList.toggle("checked");
+        let target = e.target;
+        if (target.classList.contains("task-item")) {
+            tasksData = tasksData.map((el) => {
+                if (el.id === parseInt(target.dataset.id)) {
+                    el.complete = !el.complete;
+                }
+                return el;
+            });
+            renderTask();
             saveData();
             return;
         }
-        if (e.target.className === "del-btn") {
-            e.target.parentElement.remove();
+        if (target.className === "del-btn") {
+            tasksData = tasksData.filter((el) => {
+                return el.id !== parseInt(target.dataset.id);
+            });
+            renderTask();
             saveData();
             return;
         }
@@ -47,11 +89,10 @@ tasksBox.addEventListener(
 );
 
 const saveData = () => {
-    localStorage.setItem("data", tasksBox.innerHTML);
+    localStorage.setItem("data", JSON.stringify(tasksData));
 };
 
 const getData = () => {
-    tasksBox.innerHTML = localStorage.getItem("data");
+    tasksData = JSON.parse(localStorage.getItem("data") || []);
+    renderTask();
 };
-
-getData();
